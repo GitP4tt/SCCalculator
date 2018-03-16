566efefad42f5baf6e7943afd9ee3dd6aab4f9e9
🔨👨🏿‍🍳 Bender <patrice.bender@hotmail.de>
2018-03-16 13:26:23 +0100

    Initial Commit

---
+++ SCCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SCCalculator
+//
+//  Created by Patrice Bender on 16.03.18.
+//  Copyright © 2018 Patrice Bender. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
