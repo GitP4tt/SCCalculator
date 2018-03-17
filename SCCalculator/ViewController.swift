04efbb1b5c39e0d4d5fbddb20750c7cbffe93e86
Patrice Bender <info@patrice.codes>
2018-03-17 12:36:20 +0100

    Initial Commit. Calculator for making the cashier's life easier @ schwarzwald-stadion

--- SCCalculator/ViewController.swift
+++ SCCalculator/ViewController.swift
@@ -9,6 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mulitplierView: UIStackView!
+    var currentPrice = 0.0
+    var currentMultiplier = 1
+    @IBOutlet weak var calculatorLabel: UILabel!
+    @IBOutlet weak var bestellungen: UILabel!
+    
+    var pricelist = ["Bier": 5.2,
+                            "Soft": 4.4,
+                            "Glueh": 4.4,
+                            "Kaffee": 2,
+                            "Tee": 3,
+                            "Wein": 4.4,
+                            "Wurst": 2.8,
+                            "Merquez": 3.4]
+
+   
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        let product = sender.currentTitle!
+        let positionPrice = Double(currentMultiplier)*pricelist[product]!
+        let position = (String(currentMultiplier) + " x " + String(product) + " = " + String(positionPrice))
+
+       currentPrice = currentPrice + positionPrice
+        
+        
+        calculatorLabel.text = String(currentPrice)
+        bestellungen.text = (bestellungen.text! + "\n" + position)
+
+        mulitplierView.isUserInteractionEnabled = true
+        
+    
+        
+        currentMultiplier = 1
+        
+    }
+    
+    @IBAction func multiplierChanged(_ sender: UIButton) {
+        mulitplierView.isUserInteractionEnabled = false
+        let  multiplier = sender.currentTitle!
+        currentMultiplier = Int(multiplier)!
+    }
+    
+    
+    @IBAction func reset(_ sender: Any) {
+        
+        currentPrice = 0
+        mulitplierView.isUserInteractionEnabled = true
+        calculatorLabel.text = String(currentPrice)
+        bestellungen.text = ""
+
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +75,6 @@ class ViewController: UIViewController {
     }
 
 
+   
 }
 
