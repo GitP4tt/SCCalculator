7e13acacff26d7ccd3875feca7e8d400c1ce862d
Patrice Bender <info@patrice.codes>
2018-04-24 00:03:15 +0200

    Added settings to slide out menu

---
+++ SCCalculator/Controller/SettingsController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsController.swift
+//  SCCalculator
+//
+//  Created by Patrice Bender on 23.04.18.
+//  Copyright © 2018 Patrice.Codes. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if self.revealViewController() != nil {
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+}
