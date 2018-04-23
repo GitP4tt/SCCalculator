492ffdfc2f6b1e7db30ce5bd512f02d1b2acc6c1
Patrice Bender <info@patrice.codes>
2018-04-23 22:30:56 +0200

    Added second menu item to menuController

---
+++ SCCalculator/Controller/OverviewController.swift
@@ -0,0 +1,22 @@
+//
+//  OverviewController.swift
+//  SCCalculator
+//
+//  Created by Patrice Bender on 23.04.18.
+//  Copyright © 2018 Patrice.Codes. All rights reserved.
+//
+
+import UIKit
+
+class OverviewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if self.revealViewController() != nil {            
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+
+}
